ec62b06ae99281a6368e38cf580ac3421803e7c5
Jenny Kim <yk494@cornell.edu>
2015-09-04 10:50:49 -0400

    faster slide

--- app/src/main/java/io/toasterapp/toaster_tabs/PostShowActivity.java
+++ app/src/main/java/io/toasterapp/toaster_tabs/PostShowActivity.java
@@ -109,9 +109,9 @@ public class PostShowActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
